5b826a4d0fb7fc8210de34339f819cb724f36250
liveseasonal <matt@liveseasonal.com>
2017-12-28 22:14:06 -0800

    Added some js for a search overlay

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,7 +1,38 @@
+import $ from 'jquery'; 
+
 class Search {
+
+// 1. Describe and create/initite our object
+
   constructor(){
-    alert("Hello I am a search");
+    this.openButton = $('.js-search-trigger');
+    this.closeButton = $('search-overlay__close')
+    this.searchOverlay = $('.search-overlay')
+    this.events();
   }
+
+//2. Events List all of my different events
+
+events() {
+  this.openButton.on('click',this.openOverlay.bind(this));
+  this.closeButton.on('click',this.closeOverlay.bind(this));
+}
+
+
+
+//3. Methods (function, action...)
+
+
+
+  openOverlay() {
+    this.searchOverlay.addClass('search-overlay--active');
+
+  }
+
+  closeOverlay() {
+    this.searchOverlay.removeClass('search-overlay--active');
+  }
+
 }
 
 export default Search; 
@@ -11,3 +42,8 @@ export default Search;
 // So when creating a class in JS always have the constructor method and whatever is between 
 // the curly brackets will run 
 // When we want to export to another file to use we use export default Search; 
+
+
+
+
+
--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -10545,13 +10545,55 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Search = function Search() {
-  _classCallCheck(this, Search);
+var Search = function () {
 
-  alert("Hello I am a search");
-};
+  // 1. Describe and create/initite our object
+
+  function Search() {
+    _classCallCheck(this, Search);
+
+    this.openButton = (0, _jquery2.default)('.js-search-trigger');
+    this.closeButton = (0, _jquery2.default)('search-overlay__close');
+    this.searchOverlay = (0, _jquery2.default)('.search-overlay');
+    this.events();
+  }
+
+  //2. Events List all of my different events
+
+  _createClass(Search, [{
+    key: 'events',
+    value: function events() {
+      this.openButton.on('click', this.openOverlay.bind(this));
+      this.closeButton.on('click', this.closeOverlay.bind(this));
+    }
+
+    //3. Methods (function, action...)
+
+
+  }, {
+    key: 'openOverlay',
+    value: function openOverlay() {
+      this.searchOverlay.addClass('search-overlay--active');
+    }
+  }, {
+    key: 'closeOverlay',
+    value: function closeOverlay() {
+      this.searchOverlay.removeClass('search-overlay--active');
+    }
+  }]);
+
+  return Search;
+}();
 
 exports.default = Search;
 
@@ -13502,7 +13544,6 @@ var mobileMenu = new _MobileMenu2.default();
 var heroSlider = new _HeroSlider2.default();
 var googleMap = new _GoogleMap2.default();
 var search = new _Search2.default();
-var amazingSearch = new _Search2.default();
 
 // So we have imported a our search.js javascript from another within the same modules 
 // folder hence the ./modules. 
